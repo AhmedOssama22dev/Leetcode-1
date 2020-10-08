6160e723eb9fa9516442253ece8130839efef883
Ahmed Ossama <58595131+AhmedOssama22dev@users.noreply.github.com>
2020-10-08 02:02:45 +0200

    Create Peak Index in a Mountain Array[852].py

---
+++ Peak Index in a Mountain Array[852].py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        return arr.index(max(arr))
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        
